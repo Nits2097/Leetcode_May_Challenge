415b0be48cb86d36383778f36a241458b2df39ca
Niti Shah <nits2097@gmail.com>
2020-05-10 11:19:37 -0700

    code

---
+++ Day10_FindTheTownJudge.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int arr[] = new int[N+1];
+        for(int i=0;i<trust.length;i++){
+            arr[trust[i][0]]--;
+            arr[trust[i][1]]++;
+        }
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==N-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
